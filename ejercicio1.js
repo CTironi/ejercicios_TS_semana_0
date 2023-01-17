23a8fdcda59785a0f5738c5b397f5002912147e0
CTironi <tironi741@gmail.com>
2023-01-17 20:49:21 -0300

    Ejercicio 1 listo

--- ejercicio1.js
+++ ejercicio1.js
@@ -4,9 +4,7 @@
 Crear una función que calcule el área de un círculo dado su radio. Utiliza la fórmula: A = π * r^2
 
 */
-
-function area(r : number){
-    return Math.PI * Math.pow(r , 2)
+function area(r) {
+    return Math.PI * Math.pow(r, 2);
 }
-
-console.log(`El área del círculo es: ${area(5)}`);
+console.log("El \u00E1rea del c\u00EDrculo es: ".concat(area(5)));
